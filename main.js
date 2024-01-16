ccc35ceb1a6640f72b42453e17369af2a68f5f71
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-16 15:30:42 +0100

    feat: genero le foto tramite JS

---
+++ main.js
@@ -0,0 +1,28 @@
+const photoContainer = document.getElementById("slideContainer")
+const thumbnailsContainer = document.getElementById("thumbnailsContainer")
+const next = document.getElementById("next");
+const back = document.getElementById("back");
+const allPhotos = document.getElementsByClassName("slide");
+
+//* On Load 
+let fotoLibrary = ["01.webp","02.webp","03.webp","04.webp","05.webp"];
+let activeImage = 1;
+
+//* genero photo
+
+const activeImageUrl = fotoLibrary[activeImage];
+console.log(activeImageUrl);
+const photoHtml =`<img src="./img/${activeImageUrl}" alt="">`;
+photoContainer.innerHTML = photoHtml;
+
+//*gerenero foto nel thumbnails
+let thumbHtml = "";
+for(i = 0; i < fotoLibrary.length; i++ ){
+    const image = fotoLibrary[i];
+    if(i != 2 && i != 3){
+        thumbHtml += `<div class="brd box "><img src="./img/${image}" alt=""></div>`;
+    }else{
+        thumbHtml += `<div class="brd box special"><img src="./img/${image}" alt=""></div>`;
+    }
+};
+thumbnailsContainer.innerHTML = thumbHtml;
