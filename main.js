e35f5cdbe423005b5bdb482dd87ee82fa706bfd6
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-23 07:40:18 +0100

    fix bug go to next slide

--- main.js
+++ main.js
@@ -3,44 +3,74 @@ const container = document.getElementById("container");
 const thumbnailsContainer = document.getElementById("thumbnailsContainer");
 const next = document.getElementById("next");
 const back = document.getElementById("back");
-const allPhotos = document.getElementsByClassName("box");
+const allPhotosthums = document.getElementsByClassName("box");
+const allPhotosSlides = document.getElementsByClassName("slide");
+
+
 
 //* On Load 
 let fotoLibrary = ["01.webp","02.webp","03.webp","04.webp","05.webp"];
+let ultimaFoto = fotoLibrary.length;
 let activeImage = 0;
-container.addEventListener("mouseout", function(){
-    console.log("mouse over");
-    clearInterval(loop);
-});
+generoPhoto();
+
+
+
+
 //* genero photo
-
-const activeImageUrl = fotoLibrary[activeImage];
-const photoHtml =`<img src="./img/${activeImageUrl}" alt="">`;
-photoContainer.innerHTML = photoHtml;
-
-//*gerenero foto nel thumbnails
-let thumbHtml = "";
-for(i = 0; i < fotoLibrary.length; i++ ){
-    const image = fotoLibrary[i];
-    if(i == 0){
-        thumbHtml += `<div class=" box "><img src="./img/${image}" alt=""></div>`;
-    }
-    else if(i != 2 && i != 3){
-        thumbHtml += `<div class=" box in-active-box"><img src="./img/${image}" alt=""></div>`;
-    }else{
-        thumbHtml += `<div class=" box special in-active-box"><img src="./img/${image}" alt=""></div>`;
-    }
+function generoPhoto(){
+    let thumbHtml = "";
+    let photoHtml = '';
+    for(i = 0; i <= ultimaFoto - 1; i++ ){
+        const image = fotoLibrary[i];
+        if(i == 0){
+            photoHtml += `<div class="slide active-slide"><img src="./img/${image}" alt=""></div>`;
+            thumbHtml += `<div class="box active-box"><img src="./img/${image}" alt=""></div>`;
+        /*}
+        else if(i != 3 && i != 4){
+            photoHtml += `<div class="slide"><img src="./img/${image}" alt=""></div>`;
+            thumbHtml += `<div class=" box"><img src="./img/${image}" alt=""></div>`;*/
+        }else{
+            photoHtml += `<div class="slide"><img src="./img/${image}" alt=""></div>`;
+            thumbHtml += `<div class=" box special"><img src="./img/${image}" alt=""></div>`;
+        };
+    };
+    photoContainer.innerHTML = photoHtml;
+    thumbnailsContainer.innerHTML = thumbHtml;
 };
-thumbnailsContainer.innerHTML = thumbHtml;
 
 
 
 //** sul click del next
-const loop = next.addEventListener("click", function(){
+next.addEventListener("click", function(){
+    
+     goToSlide(next);
 
-    setInterval(function(){
-        goToSlide(); 
-    }, 1000);
+    
+    /*
+    if(activeImage == fotoLibrary.length){
+        //* elemento presente
+        allPhotosSlides[fotoLibrary.length -1].classList.remove("active-slide");
+        allPhotosthums[fotoLibrary.length -1].classList.remove("active-box");
+        //* elemento seguente
+        allPhotosSlides[0].classList.add("active-slide");
+        allPhotosthums[0].classList.add("active-box");
+        activeImage = 0;
+        
+    }else{
+        //* elemento presente
+        allPhotosSlides[activeImage - 1].classList.remove("active-slide");
+        allPhotosthums[activeImage -1].classList.remove("active-box");
+
+        //* elemento seguente
+        allPhotosSlides[activeImage].classList.add("active-slide");
+        allPhotosthums[activeImage].classList.add("active-box");
+    };
+
+   
+
+    activeImage ++;
+    console.log(activeImage);*/
    
 });
 
@@ -48,42 +78,38 @@ const loop = next.addEventListener("click", function(){
 
 //** sul click del back
 back.addEventListener("click", function(){
-    if(activeImage == 0){
-        activeImage = fotoLibrary.length   ;
-    }
-    activeImage --;
-    const activeImageUrl = fotoLibrary[activeImage];
-    const photoHtml =`<img src="./img/${activeImageUrl}" alt="">`;
-    photoContainer.innerHTML += photoHtml;
-    //*thumbImg
-    if(activeImage == fotoLibrary.length  - 1){
-        allPhotos[0].classList.add("in-active-box");
-    }
-    allPhotos[activeImage].classList.remove("in-active-box");
-    allPhotos[activeImage + 1 ].classList.add("in-active-box");
+   // goToSlide();
     
-});
+})
 
 //*** Funzioni */
 
-function goToSlide(){
+function goToSlide(direction){
+    let oldActiveImage;
+    let newActiveImage;
+    
+    oldActiveImage = activeImage;
+    newActiveImage = activeImage + 1 ;
+    
+    console.log(activeImage);
+
+    //* elemento presente
+    allPhotosSlides[oldActiveImage ].classList.remove("active-slide");
+    allPhotosthums[oldActiveImage ].classList.remove("active-box");
+    if (newActiveImage === ultimaFoto) {
+        newActiveImage = 0;
+    }
+    console.log(allPhotosSlides[oldActiveImage]);
+    console.log("-----------------------")
+    //* elemento seguente
+    allPhotosSlides[newActiveImage].classList.add("active-slide");
+    allPhotosthums[newActiveImage].classList.add("active-box");
+    console.log(allPhotosSlides[newActiveImage]);
     activeImage ++;
-    //*img
-    const activeImageUrl = fotoLibrary[activeImage];
-    //* slide img
-    const photoHtml =`<img src="./img/${activeImageUrl}" alt="">`;
-    photoContainer.innerHTML += photoHtml;
-    //*thumbImg
-    allPhotos[activeImage].classList.remove("in-active-box");
-    if(activeImage == 0){
-        allPhotos[fotoLibrary.length - 1].classList.add("in-active-box");  
-    }
-    allPhotos[activeImage -1 ].classList.add("in-active-box");
-
-    if(activeImage == fotoLibrary.length - 1){
-        activeImage = -1;
-    }
-
-}
+    if(activeImage === ultimaFoto){
+        activeImage = 0;
+    };
+    
+};
 // Todo: riscrivere la funzione per generare le img
     
